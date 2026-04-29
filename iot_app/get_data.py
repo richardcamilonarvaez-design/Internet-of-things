fb28d73ca3cbaa37baf1f9a65b3b921d2d4f839e
Richard Camilo Narvaez <richardcamilonarvaez@gmail.com>
2026-04-29 13:51:32 -0500

    Corrections get data

--- iot_app/get_data.py
+++ iot_app/get_data.py
@@ -1,16 +1,13 @@
 import serial
 import time
-
-arduino_port= 'COM6'
-baud_rate= '9600'
-
+arduino_port = 'COM3' 
+baud_rate = 9600
 ser = serial.Serial(
-    arduino_port,
-    baud_rate,
+    arduino_port, 
+    baud_rate, 
     timeout=1
 )
-
-time.sleep(2)
+time.sleep(2) 
 while True:
-    data=ser.readline().decode('utf-8').rstrip()
-    print(data)
+    data = ser.readline().decode('utf-8').rstrip();
+    print(data);
--- iot_app/weather_station/models.py
+++ iot_app/weather_station/models.py
@@ -4,4 +4,3 @@ from django.db import models
 
 class Sensor(models.Model):
     name=models.CharField(max_length=50)
-    humidity=models.FloatField(default=0)
